37828b26959070caca6b04e7122060589f81732c
Kmoran1512 <kmoran.1512@gmail.com>
2020-04-05 15:04:20 -0400

    Conversion to private

---
+++ atom.js
@@ -0,0 +1,10 @@
+var config = {
+    API_KEY : "AIzaSyD5lIj7v8PqVrVhz-k2JUXvBv2e6gkpAwI",
+    DOMAIN: "math-battle-a9de2.firebaseapp.com",
+    DATA_URL: "https://math-battle-a9de2.firebaseio.com",
+    P_ID: "math-battle-a9de2",
+    BUCKET: "math-battle-a9de2.appspot.com",
+    SENDER_ID: "108817153492",
+    APP_ID: "1:108817153492:web:9d68dbf5f2758d2f9e6a41",
+    MEASUREMENT_ID: "G-D2HWCRF0Y0"
+}
